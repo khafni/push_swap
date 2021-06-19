47f6702843414a558f4e08b1441d5d0e47f20ce5
Khalil Hafni <khafni@e1r7p4.1337.ma>
2021-06-19 11:53:56 +0100

    lol

--- chunking.c
+++ chunking.c
@@ -17,6 +17,7 @@ t_chunk_elem elem_chunk(int value, int chunk_num)
 	celm->chunk_num = chunk_num;
 	return (celm);
 }
+
 //void chunk_from_sa_to_sb(t_dlist st_a, t_dlist st_b);
 
 int calculate_n_of_chunks(t_dlist stack)
@@ -40,6 +41,22 @@ int calculate_n_of_chunks(t_dlist stack)
 	return (n_chunks);
 }
 
+t_arrptr fill_sorted_array_from_chnk(t_dlist stack)
+{
+	int which_chunk;
+	t_arrptr arr;
+
+	arr = empty_arrptr_create(NULL);
+	which_chunk = ((t_chunk_elem)(stack->cursor_p->value))->chunk_num;
+	while (stack->cursor_p != stack->sentinel)
+	{
+		if (which_chunk != ((t_chunk_elem)(stack->cursor_p->value))->chunk_num)	
+			break;	
+		arrptr_add(arr, stack->cursor_p->value);
+		dlist_move_cursor_to_previous(stack);
+	}
+	return (arr);
+}
 
 void chunk_from_sa_to_sb(t_dlist st_a, t_dlist st_b)
 {
